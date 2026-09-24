60d72168ffaaf14ba0b35af25e3e3e8be692ce39
swap2you <swap2you@gmail.com>
2026-09-23 21:58:56 -0400

    test: harden forecast edge cases

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -7,3 +7,6 @@ def test_bounded_forecast_returns_interval_and_requires_minimum():
  x=bounded_forecast(pd.DataFrame({'period_id':[1,2,3,4,5,6],'enrolled':[10,12,14,16,18,20]}))
  assert x['available'] and x['lower_bound'] <= x['estimate'] <= x['upper_bound'] and x['observations']==6
  assert bounded_forecast(pd.DataFrame({'period_id':[1,2,3],'enrolled':[10,12,14]}))['available'] is False
+def test_bounded_forecast_handles_nulls_missing_periods_and_negative_extrapolation():
+ x=bounded_forecast(pd.DataFrame({'period_id':[1,3,4,7,8,10,11],'enrolled':[100,None,80,60,40,20,10]}))
+ assert x['available'] and x['forecast_period_id']==12 and x['lower_bound']>=0
